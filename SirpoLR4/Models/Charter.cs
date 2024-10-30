dadb7d444cadafa15d7a978a9cea2fb79ec6ced3
Matvey <sinema3d00@gmail.com>
2024-10-30 19:09:15 +0300

    add model

---
+++ SirpoLR4/Models/Charter.cs
@@ -0,0 +1,11 @@
+﻿namespace SirpoLR4.Models
+{
+    public class Charter
+    {
+        public Guid Id { get; set; }
+        public string? CititesPath { get; set; }
+        public int Price { get; set; }
+        public DateTime CharterDateTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
