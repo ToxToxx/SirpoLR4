673d65c352ca7c9ac2f6e18c4521c14111ac244a
Matvey <sinema3d00@gmail.com>
2024-10-31 20:04:15 +0300

    remake controller for our purposes

--- SirpoLR4/Controllers/HomeController.cs
+++ SirpoLR4/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SirpoLR4.DataAccess;
 using SirpoLR4.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,18 @@ namespace SirpoLR4.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CharterDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CharterDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var charters = await _context.Charters.AsNoTracking().ToListAsync();
+            return View(charters);
         }
 
         public IActionResult Privacy()
