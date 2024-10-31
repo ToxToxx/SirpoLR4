77ed26632947398004b08bfec226bacd88cc423f
Matvey <sinema3d00@gmail.com>
2024-10-31 20:03:58 +0300

    add db context

---
+++ SirpoLR4/DataAccess/CharterDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SirpoLR4.Models;
+using System.Collections.Generic;
+
+namespace SirpoLR4.DataAccess
+{
+    public class CharterDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CharterDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Charter> Charters => Set<Charter>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Database"));
+        }
+    }
+}
